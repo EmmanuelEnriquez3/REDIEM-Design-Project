b6413904c500305fd94281340a2de779590a4cdb
Emmanuel Enriquez <63181392+EmmanuelEnriquez3@users.noreply.github.com>
2022-01-22 13:52:23 -0700

    Add files via upload

---
+++ RediemStoryboard/rediemStoryboard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  rediemStoryboard
+//
+//  Created by Emanuel Enriquez on 1/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+ 
+    @IBAction func tapGesture(_ sender: Any) {
+    }
+    
+}
+
